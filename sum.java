fe9db41b3ce3429393836d0ce67add8e21e217f1
Rishabhsingh715 <srishabh715@gmail.com>
2021-08-02 15:25:39 +0530

    added

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class Sum{
+
+public static void main(String[] args){
+  
+  System.out.println("Don't leave me behind because good music is so hard to find\n"+" "+"The sum is 786 ");
+}
+
+
+}
