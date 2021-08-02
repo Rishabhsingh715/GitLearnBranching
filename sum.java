59052ad616c6548999fcca458f984c0bb30c928f
Rishabhsingh715 <srishabh715@gmail.com>
2021-08-02 15:38:35 +0530

    committing the file in the dev branch

--- sum.java
+++ sum.java
@@ -2,8 +2,7 @@ public class Sum{
 
 public static void main(String[] args){
   
-  System.out.println("Don't leave me behind because good music is so hard to find\n"+" "+"The sum is 786 ");
-}
+  System.out.println("Don't leave me behind because good music is so hard to find\n");
 
 
 }
